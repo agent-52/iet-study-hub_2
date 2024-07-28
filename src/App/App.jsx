70087d1623b74c6d0a609a7f0e882c8d81bba7b1
agent-52 <abhaybhadauriya52@gmail.com>
2024-07-28 23:13:48 +0530

    add the snake effects to year1 and year2 and redirect to sorry page on year3 and year4

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -13,6 +13,20 @@ import "../Page2/Page2.css"
 
 const App = () =>
 {
+  useEffect(()=>{
+    document.querySelector(".year1").addEventListener("click", ()=>{
+      document.querySelector(".year1").classList.add("snake")
+      document.querySelector(".year1>.yearCount").classList.add("text-green-a9")
+      document.querySelector(".year2").classList.remove("snake")
+      document.querySelector(".year2>.yearCount").classList.remove("text-green-a9")
+    })
+    document.querySelector(".year2").addEventListener("click", ()=>{
+      document.querySelector(".year2").classList.add("snake")
+      document.querySelector(".year2>.yearCount").classList.add("text-green-a9")
+      document.querySelector(".year1").classList.remove("snake")
+      document.querySelector(".year1>.yearCount").classList.remove("text-green-a9")
+    })
+  })
   const [year1Array, setYear1Array] = useState(year1Batch1Array)
   const updateYear1Array = (array) =>{
     setYear1Array(array)
@@ -36,25 +50,25 @@ const App = () =>
           </div>
           <div className="yearBox text-slate-12 text-sm0">
             <Link className="textDecNone" to="/year1">
-              <div className="bigBox">
+              <div className="bigBox year1">
                 <div className="yearCount text-green-a9">1</div>
                 <div >First Year</div>
               </div>
             </Link>
             <Link className="textDecNone" to="/year2/cse/batch1">
-              <div className="bigBox">
+              <div className="bigBox year2">
                 <div className="yearCount">2</div>
                 <div>Second Year</div>
               </div>
             </Link>
-            <div className="bigBox">
+            <Link className="textDecNone" to="/sorry"><div className="bigBox">
               <div className="yearCount">3</div>
               <div >Third Year</div>
-            </div>
-            <div className="bigBox">
+            </div></Link>
+            <Link className="textDecNone" to="/sorry"><div className="bigBox">
               <div className="yearCount">4</div>
               <div >Fourth Year</div>
-            </div>
+            </div></Link>
           </div>
         </div>
         <div className="p2Grid">
