9e9bd8545f5617b8f93ef2ef9f1a74035722c21d
agent-52 <abhaybhadauriya52@gmail.com>
2024-08-01 21:08:37 +0530

    fix spelling of gangrade

--- src/Team/Team.jsx
+++ src/Team/Team.jsx
@@ -234,7 +234,7 @@ const Team = () =>
                     <div className="flexC gap1">
                         <div><span>Abhinav Sahu (2022-2026)</span> for providingpyq's and notes of cse SEM-III </div>
                         <div><span>Kashish Jakhodiya (2023-2027) </span> for providing notes of cse SEM-III</div>
-                        <div><span>Shruti Gangeade (2022-2026)  </span> for providing notes of entc SEM-III</div>
+                        <div><span>Shruti Gangrade (2022-2026)  </span> for providing notes of entc SEM-III</div>
                         <div><span>Vidhan Agrawal (2023-2027) </span> for providing notes of it SEM-III</div>
                     </div>
                     <a href="https://www.instagram.com/ietstudyhub?igsh=MWR5b3FnbWoyNTl6aQ==" className="textDecNone"><Button text="Contribute >" version={1} /></a>
